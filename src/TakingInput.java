d1bebe13540dd637470ed3b37118438358080b95
Tejas Lale <tejaslale29@gmail.com>
2024-01-13 09:19:41 +0530

    Initial commit

---
+++ src/TakingInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class TakingInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entre your lucky number");
+        int num_1 = sc.nextInt();
+        System.out.println("Lucky NUmber is :" +num_1);
+
+    }
+}
